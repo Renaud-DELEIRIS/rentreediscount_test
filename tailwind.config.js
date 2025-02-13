cef4750b3ab35b60a5e29ea36fa5c431a5594e76
Renaud-DELEIRIS <renaud.de-leiris@epitech.eu>
2025-02-13 17:20:31 +0100

    with html template

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,30 @@ module.exports = {
       fontFamily: {
           alamain: ['Alamain', 'sans-serif'],
           asap: ['Asap', 'sans-serif'],
+          asap_sups: ['Asap', {
+            fontFeatureSettings: "'sups' on",
+          }],
       },
       boxShadow: {
         headerShadow: '3px 0px 6px 0px rgba(0, 0, 0, 0.15)',
+        ctaButton: '0px 2px 2px 0px rgba(0, 0, 0, 0.15)',
+        shopCard: '0px 0px 6px 0px rgba(0, 0, 0, 0.15)',
       },
       colors: {
         'font': {
           text: {
             "black": "#000000",
-            "muted": "#757575"
+            "muted": "#757575",
           } 
         },
         'primary/orange': {
           "DEFAULT": "#FE6400",
-          "dark": "#E94308"
+          "dark": "#E94308",
+          "light": "#FBA36B",
         },
         'secondary/orange_medium': "#FAEEE5",
         "secondary/orange_light": "#FCF5F0",
+        'secondary/orange_dark': "#FEE7D9",
         "states": {
           "error-promo": "#EA1313"
         }
